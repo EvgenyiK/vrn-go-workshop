bb4d4f1539e7c4d422bdfc1cd083ab16d8af43c1
Evgen <evgenprogrammer@gmail.com>
2020-04-26 21:06:39 +0300

    mock

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -21,7 +21,7 @@ func main() {
 
 	apiClient := jokes.NewJokeClient(cfg.JokeURL)
 
-	h := handler.NewHandler()
+	h := handler.NewHandler(apiClient)
 	r := chi.NewRouter()
 	r.Get("/hello", h.Hello)
 	path := cfg.Host + ":" + cfg.Port
