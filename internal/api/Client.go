08527e1a2ffd66ee1cca2862aa3a360979216fa3
Evgen <evgenprogrammer@gmail.com>
2020-04-26 11:31:37 +0300

    add handler

--- internal/api/Client.go
+++
@@ -1,7 +0,0 @@
-package api
-
-// Client interacts with 3-rd party joke API
-type Client interface {
-	// GetJoke returns one  joke
-	GetJoke() (*JokeResponse, error)
-}
--- internal/api/models.go
+++
@@ -1,6 +0,0 @@
-package api
-
-// JokeResponse is joke API response type
-type JokeResponse struct {
-	Joke string `json:"joke"`
-}
