1e3b542b03c8e4d3481c2cb187fa7a8de9bb5b43
Evgen <evgenprogrammer@gmail.com>
2020-04-26 17:11:40 +0300

    add joke API

---
+++ internal/api/client.go
@@ -0,0 +1,6 @@
+package api
+
+//Client interacts with 3-rd party joke API
+type Client interface {
+	GetJoke() (*JokeResponse, error)
+}
---
+++ internal/api/models.go
@@ -0,0 +1,7 @@
+package api
+
+// JokeResponse is joke API response type
+
+type JokeResponse struct {
+	Joke string `json:"joke"`
+}
