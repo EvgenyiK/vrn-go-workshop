28296449dbfd2f380d122d7f6fbce084e0f18a21
Evgen <evgenprogrammer@gmail.com>
2020-04-27 19:13:48 +0300

    add gracefull shutdown

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -1,14 +1,19 @@
 package main
 
 import (
-	"github.com/go-chi/chi"
-	"github.com/ilyakaznacheev/cleanenv"
-	"workshop/internal/api/jokes"
-	"workshop/internal/config"
-
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
+	"github.com/go-chi/chi"
+	"github.com/ilyakaznacheev/cleanenv"
+
+	"workshop/internal/api/jokes"
+	"workshop/internal/config"
 	"workshop/internal/handler"
 )
 
@@ -21,14 +26,36 @@ func main() {
 
 	apiClient := jokes.NewJokeClient(cfg.JokeURL)
 
-	h := handler.NewHandler(apiClient)
+	h := handler.NewHandler(apiClient, cfg.CustomJoke)
+
 	r := chi.NewRouter()
+
 	r.Get("/hello", h.Hello)
+
 	path := cfg.Host + ":" + cfg.Port
 
-	log.Printf("starting server at %s", path)
-	err = http.ListenAndServe(path, r)
-	log.Fatal(err)
+	srv := &http.Server{
+		Addr:    path,
+		Handler: r,
+	}
 
-	log.Print("shutting server down")
+	// handle shutdown gracefully
+	quit := make(chan os.Signal, 1)
+	done := make(chan error, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-quit
+		ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+		defer cancel()
+		err := srv.Shutdown(ctx)
+		//....
+		done <- err
+	}()
+
+	log.Printf("starting server at %s", path)
+	_ = srv.ListenAndServe()
+
+	err = <-done
+	log.Printf("shutting server down with %v", err)
 }
